e31aa11ea71b620cdd63e7bf3604fe148743fe6a
Lawi-Mwaura <Lawimwaura@gmail.com>
2023-08-29 11:57:41 +0300

    Recursion errors

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,5 +1,9 @@
 def write_file(file_name, file_content):
-    pass
+     write_file(file_name="test_file", file_content="This is a test content.")
+     with open(file_name, "w") as file:
+            file.write(file_content)
+           
+
 
 def append_file(file_name, append_content):
     pass
